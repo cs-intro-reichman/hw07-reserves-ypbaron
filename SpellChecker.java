6822be6874b057e704c7098e4bf98ce2facdcb0b
Yuval Baron <yuval.baron@post.runi.ac.il>
2024-03-15 00:03:57 +0200

    HW07

--- SpellChecker.java
+++ SpellChecker.java
@@ -3,34 +3,40 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word = args[0];
-		int threshold = Integer.parseInt(args[1]);
-		String[] dictionary = readDictionary("dictionary.txt");
-		String correction = spellChecker(word, threshold, dictionary);
-		System.out.println(correction);
+		// String word = args[0]; // "hello";
+		// String word1 = "concensus";
+		// String word2 = "consensus";
+
+		// System.out.println(levenshtein(word1, word2));
+
+		// int threshold = Integer.parseInt(args[1]);// 2; //
+		// String[] dictionary = readDictionary("dictionary.txt");
+		// String correction = spellChecker(word, threshold, dictionary);
+		// System.out.println(correction);
 	}
 
 	public static String tail(String str) {
-		String tailImplementation = str.substring(1);// Your code goes here
-		return tailImplementation;
+		if (str.length() == 0) {
+			return "";// Your code goes here
+		}
+		return str.substring(1);// Your code goes here
 	}
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
-		String lowerWord1 = word1.toLowerCase();
-		String lowerWord2 = word2.toLowerCase();
-		String tail1 = tail(lowerWord1);// calls tails
-		String tail2 = tail(lowerWord2);// calls tails
+		word1.toLowerCase();
+		word2.toLowerCase();
+		String tail1 = tail(word1);// calls tails
+		String tail2 = tail(word2);// calls tails
 
-		if (word1.length() == 0)
-			return word2.length();
-		if (word2.length() == 0)
+		if (word2.length() == 0) {
 			return word1.length();
-
-		if (lowerWord1.charAt(0) == word2.charAt(0)){
+		} else if (word1.length() == 0) {
+			return word2.length();
+		} else if (word1.charAt(0) == word2.charAt(0)) {
 			return levenshtein(tail1, tail2);
 		}
-		return 1 + Math.min(levenshtein(tail1, lowerWord2), Math.min(levenshtein(lowerWord1, tail2), levenshtein(tail1, tail2)));	// go over
+		return 1 + Math.min(Math.min(levenshtein(tail1, word2), levenshtein(word1, tail2)), levenshtein(tail1, tail2));
 	}		
 		
 
